9b6dc2f76bcf97d699a9ed855b03cf0472f33d56
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-07 15:11:58 +0100

    Add files via upload

---
+++ launch.py
@@ -0,0 +1,13 @@
+import os
+from FARMWARE import MyFarmware
+from farmware_tools import log
+
+FARMWARE_NAME = "jhempbot"
+
+def main():
+    log("jhempbot --> hello")
+    farmware = MyFarmware(FARMWARE_NAME)
+    farmware.run()
+
+if __name__ == "__main__":
+main()
