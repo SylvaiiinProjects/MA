4225dd05ce61ddabe461468a52d22a0e765ebc2a
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-07 15:48:37 +0100

    Add files via upload

--- FARMWARE.py
+++ FARMWARE.py
@@ -20,3 +20,6 @@ class MyFarmware():
     def run(self):
         log("Test two move absolute", message_type='debug')
 	self.move(150,150,10,50)
+        p= [[100,100,-30],[50,50,50]]
+        self.move(p[0][0], p[0][1], p[0][2], 20)
+        self.move(p[1][0], p[1][1], p[1][2], 50)
