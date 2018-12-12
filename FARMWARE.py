760e7ce83fb2b8d6a80aeba3d79e987f48525134
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-12 09:57:21 +0100

    Add files via upload

--- FARMWARE.py
+++ FARMWARE.py
@@ -278,6 +278,7 @@ class MyFarmware():
     TOKEN = ''
     def __init__(self,farmwarename):
         self.farmwarename = farmwarename
+	self.x = coords[0]
 
     ##FUNCTION CONTROL
     def waterSensor(self):
@@ -425,8 +426,8 @@ class MyFarmware():
 	
 	#Sequence5
 	s5 = Sequence("5", "green")
-	s5.add(log("Try to move to coords[0] {}".format(coords[0]), message_type='info'))
-	s5.add(self.move(int(coords[0]),0,0,50))
+	s5.add(log("Try to move to coords[0] {}".format(0), message_type='info'))
+	s5.add(self.move(self.x,0,0,50))
 	send(cp.create_node(kind='execute', args=s5.sequence))
 
 	#Sequence6
